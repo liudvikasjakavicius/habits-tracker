f4b267a4ab555ec2dab8d4ee5028e98c8495667c
Liudvikas Jakavicius <liudvikas@jakavicius.org>
2024-03-23 13:59:51 +0200

    Fix assets url

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: process.env.NODE_ENV === 'production' ? '/habits-tracker/' : '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined // This ensures assets are correctly placed
+      }
+    }
+  }
 })
